74a7abbd4dfbe9dcf5f8906ace0c1f64ed5c7b71
Sanskar Bosmmia <sanskarbosmia@gmail.com>
2024-04-14 23:19:44 +0530

    BrickBreaker added

---
+++ BrickBreaker/src/BrickBreaker/Main.java
@@ -0,0 +1,15 @@
+package BrickBreaker;
+import javax.swing.JFrame;
+public class Main {
+	public static void main(String[] args) {
+		JFrame obj=new JFrame();
+		Game game=new Game();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(game);
+	}
+
+}
